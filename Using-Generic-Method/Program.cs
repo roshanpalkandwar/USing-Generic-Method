7e22f5a72166f6ffa75e48b868fea9f6c484672a
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-15 22:08:57 +0530

    [Roshan]Added:Between 3 Integers find Max NUmber

---
+++ Using-Generic-Method/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Using_Generic_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" chooes number\n1. Find integer maximum number among 3 numbers");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    MaxNNumber integerMaximumNumber = new MaxNNumber();
+                    int intValue = integerMaximumNumber.PrintIntegerMaximumNumber(70, 85, 60);
+                    Console.WriteLine("Maximum number of integer is " + intValue);
+                    break;
+            }
+        }
+    }
+}
