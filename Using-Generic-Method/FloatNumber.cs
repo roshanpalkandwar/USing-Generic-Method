98658c83b3052175376985b7993a0c9c0cb9392d
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-15 22:13:46 +0530

    [Roshan]Added:Find Max NUmber Using Floats Values

---
+++ Using-Generic-Method/FloatNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_Generic_Method
+{
+    internal class FloatNumber
+    {
+        public float PrintFloatMaximumNumber(float num1, float num2, float num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0
+               || num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0
+               || num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
+            {
+                return num1;
+            }
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0
+               || num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0
+               || num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0)
+            {
+                return num2;
+            }
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0
+              || num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0
+              || num3.CompareTo(num1) > 0 && num3.CompareTo(num2) >= 0)
+            {
+                return num3;
+            }
+            return default;
+        }
+    }
+}
--- Using-Generic-Method/Program.cs
+++ Using-Generic-Method/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" chooes number\n1. Find integer maximum number among 3 numbers");
+            Console.WriteLine(" chooes number\n1. Find integer maximum number among 3 numbers\n2.using float number");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -12,6 +12,11 @@
                     MaxNNumber integerMaximumNumber = new MaxNNumber();
                     int intValue = integerMaximumNumber.PrintIntegerMaximumNumber(70, 85, 60);
                     Console.WriteLine("Maximum number of integer is " + intValue);
+                    break;
+                case 2:
+                    FloatNumber floatMaximumNumber = new FloatNumber();
+                    float floatValue = floatMaximumNumber.PrintFloatMaximumNumber(88.5f, 100.9f, 125.07f);
+                    Console.WriteLine("Maximum number of float is " + floatValue);
                     break;
             }
         }
